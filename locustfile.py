8b885ca1803a737fa852fa2c4cb0220b9871d2ff
Michael Olaogun <X00171114@myTUDublin.ie>
2025-01-04 23:22:44 +0000

    Updated locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -5,7 +5,7 @@ from locust  import HttpUser, task, between
 
 class MyUser(HttpUser):
     # Defines the wait time between tasks
-    wait_time = between(1, 5)
+ wait_time = between(1, 5)
 
     @task
     def test_endpoint(self):
