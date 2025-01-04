3186c59684a6a95c6b2d25ad379479907485f820
Michael Olaogun <X00171114@myTUDublin.ie>
2025-01-04 23:31:20 +0000

    Updated locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -4,8 +4,8 @@ from locust import HttpUser, task, between
 
 
 class MyUser(HttpUser):
-    wait_time = between(1, 5) # Defines the wait time between tasks
+    wait_time = between(1, 5)  #  Defines the wait time between tasks
 
     @task
     def test_endpoint(self):
-        self.client.get("/") # Replace '/' with the endpoint you want to test
+        self.client.get("/")  #  Replace '/' with the endpoint you want to test
